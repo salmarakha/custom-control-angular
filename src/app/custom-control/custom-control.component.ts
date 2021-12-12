f53d00ac8e4425e09e00883fe5767926abc99e52
Salma Rakha <salmarakha75@gmail.com>
2021-12-12 16:48:37 +0200

    modify custom control

--- src/app/custom-control/custom-control.component.ts
+++ src/app/custom-control/custom-control.component.ts
@@ -1,5 +1,6 @@
+import { isNgTemplate } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { InputControl } from '../models/inputControl';
 
 @Component({
@@ -9,21 +10,25 @@ import { InputControl } from '../models/inputControl';
 })
 export class CustomControlComponent implements OnInit {
 
-  @Input() controls: InputControl[] = [];
+  // @Input() controls: InputControl[] = [];
+  @Input() controls: Array<Array<any>> = [];
+
   form: FormGroup = new FormGroup({});
 
   getFormControlName = (index: number) => {
-    // new inputs are pushed to the end of the array so the order is correct
-    //console.log(Object.keys(this.form.controls)[index]);
-    console.log("getFormControlName is called!!!");
-    // Function is called with every click or keypress and for each input ????
+    console.log("getFormControlName is called!!");
+    console.log(this.form.value);
     return Object.keys(this.form.controls)[index];
   }
 
   constructor() { }
 
   ngOnInit(): void {
-    this.controls.forEach((row, index) => this.form.addControl(`name${index}`, new FormControl()));
+    this.controls.flatMap(arr => arr.filter(obj => obj.type !== 'label' && obj.type !== 'button' ))
+    .forEach(input => {
+      this.form.registerControl(input.name, new FormControl())
+      // TODO: validation .addValidators()
+    });
   }
 
   ngAfterViewInit(): void {
@@ -31,14 +36,28 @@ export class CustomControlComponent implements OnInit {
   }
 
   remove(index: number) {
+    const inputsInFormGroup = this.controls[index].filter(controls => controls.type !== 'label' && controls.type !== 'button')
+    console.log(inputsInFormGroup)
+    for (const input of inputsInFormGroup) {
+      this.form.removeControl(input.name);
+    }
     this.controls.splice(index, 1);
-    this.form.removeControl(`name${index}`);
+    console.log(this.form.controls)
   }
 
   add(index: number) {
-    this.form.addControl(`name${this.controls.length}`, new FormControl());
-    this.controls.push(this.controls[index]);
-    console.log(Object.keys(this.form.controls));
+    const inputsInFormGroup = this.controls[index].filter(controls => controls.type !== 'label' && controls.type !== 'button')
+    console.log(inputsInFormGroup)
+    for (const input of inputsInFormGroup) {
+      let control = new FormControl();
+      this.form.registerControl(input.name, control);
+      // TODO: validation
+    }
+    this.controls.push(this.controls[index]); // add the control group to the array
+    console.log("form controls", this.form.controls)
+    console.log(this.controls)
+    // console.log("controls: ", this.controls);
+    // console.log(Object.keys(this.form.controls));
   }
 
 }
