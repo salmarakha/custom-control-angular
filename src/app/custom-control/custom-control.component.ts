e754b33e4a1822375a9d353f762960ab1b572a59
Salma Rakha <salmarakha75@gmail.com>
2021-12-12 16:54:48 +0200

    fix

--- src/app/custom-control/custom-control.component.ts
+++ src/app/custom-control/custom-control.component.ts
@@ -36,28 +36,16 @@ export class CustomControlComponent implements OnInit {
   }
 
   remove(index: number) {
-    const inputsInFormGroup = this.controls[index].filter(controls => controls.type !== 'label' && controls.type !== 'button')
-    console.log(inputsInFormGroup)
-    for (const input of inputsInFormGroup) {
-      this.form.removeControl(input.name);
-    }
+    this.controls[index].filter(controls => controls.type !== 'label' && controls.type !== 'button')
+    .forEach(input => this.form.removeControl(input.name));
     this.controls.splice(index, 1);
-    console.log(this.form.controls)
   }
 
   add(index: number) {
-    const inputsInFormGroup = this.controls[index].filter(controls => controls.type !== 'label' && controls.type !== 'button')
-    console.log(inputsInFormGroup)
-    for (const input of inputsInFormGroup) {
-      let control = new FormControl();
-      this.form.registerControl(input.name, control);
-      // TODO: validation
-    }
+    this.controls[index].filter(controls => controls.type !== 'label' && controls.type !== 'button')
+    .forEach(input => this.form.registerControl(input.name, new FormControl()));
+    // TODO: validation
     this.controls.push(this.controls[index]); // add the control group to the array
-    console.log("form controls", this.form.controls)
-    console.log(this.controls)
-    // console.log("controls: ", this.controls);
-    // console.log(Object.keys(this.form.controls));
   }
 
 }
